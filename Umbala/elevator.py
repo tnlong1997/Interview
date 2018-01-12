682a49bd9086e87ad31c97631340d300049b4286
tnlong1997 <tnlong1997@gmail.com>
2018-01-12 15:29:37 +0700

    [Umbala] Elevator

---
+++ Umbala/elevator.py
@@ -0,0 +1,18 @@
+
+class Elevator():
+
+    def __init__(self, curr_floor, )
+
+def elevator():
+    inp = input("Enter number of floors: ")
+    n = int(inp)
+    inp = input("Enter number of elevators: ")
+    m = int(inp)
+    print(n, m)
+    time = 0
+    curr_floor = {}
+    s = ""
+    inp = input("Enter action at time %d", % time)
+
+
+elevator()
